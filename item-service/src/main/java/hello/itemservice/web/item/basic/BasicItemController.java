9588fc410cea1410b5463d37db3e13ac98e300eb
Raedeok-H <raedeokhwang@gmail.com>
2024-05-02 20:46:34 +0900

    상품 등록 처리 - @ModelAttribute

--- item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -35,6 +35,65 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+    //    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    /**
+     * 1. @ModelAttribute - 요청 파라미터 처리
+     *
+     * @ModelAttribute 는 Item 객체를 생성하고, 요청 파라미터의 값을 프로퍼티 접근법(setXxx)으로 입력해준다.
+     * <p>
+     * 2. @ModelAttribute - Model 추가
+     * @ModelAttribute 는 중요한 한가지 기능이 더 있는데,
+     * 바로 모델(Model)에 @ModelAttribute 로 지정한 객체를 자동으로 넣어준다.
+     * 지금 코드를 보면 model.addAttribute("item", item) 가 주석처리 되어 있어도 잘 동작하는 것을 확인할 수 있다.
+     */
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item) {
+        itemRepository.save(item);
+
+//        model.addAttribute("item", item);
+        // @ModelAttribute("item") 에서 해당 동작을 하고, Model 파라미터도 필요없다.
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        // @ModelAttribute 안의 이름을 생략하면,
+        // 객체의 첫글자를 소문자로 바꾼 이름을 자동으로 사용한다.
+        // ex) HelloData -> helloData
+
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute 자체 생략 가능
+     * model.addAttribute(item) 자동 추가
+     * 단순 타입들은 @RequestParam가 적용되고,
+     * 그 외는 @ModelAttribute 가 적용되는 것을 지난 시간에 학습했었다.
+     */
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
